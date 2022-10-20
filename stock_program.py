e834d4f49f0062d6328556cc4aee8188f8cf3212
Luke Matteoni <ljmatteoni@users.noreply.github.com>
2022-10-20 10:27:35 -0500

    Create stock_program.py

---
+++ stock_program.py
@@ -0,0 +1,32 @@
+print ("What is your Name?")
+
+name = input ("")
+
+#This is the apple stock 
+
+print ((name) + (" What price is apple stock"))
+
+apple = (168)
+
+apple_stock= int(input ("")) * (apple)
+
+print ("Your total is:")
+
+print (apple_stock)
+
+
+#This is the snap stock 
+
+print (("\n") + (name) + (" What price is snap stock?"))
+
+snap = (19)
+
+snap_stock= int(input ("")) * (snap)
+
+print ("Your total is:")
+
+print (snap_stock)
+
+print ("\nCombined total is:") 
+
+print ((snap_stock) + (apple_stock))
